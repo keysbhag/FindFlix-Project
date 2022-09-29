4d90740aaab22a99915711626fcb87161e38544d
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-09-28 22:09:30 -0400

    Added functions to my search button and passed values to the singlepage-hmtl

---
+++ assets/homepage.js
@@ -0,0 +1,11 @@
+
+let searchButton = document.getElementById('search-button');
+let searchInput = document.getElementById('search-input');
+
+searchButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    let searchVal = searchInput.value;
+    
+    window.location.replace('./single-page.html?q='+searchVal);
+    
+})
