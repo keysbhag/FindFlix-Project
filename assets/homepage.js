ae5cef6e66fb26a7a442cad319280f451c59fec5
Pak Yin Sin <Spy@MacBook-Pro.local>
2022-09-28 21:13:27 -0400

    added function to add Divs onto single-page.html to project search results

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,28 +1,44 @@
+var searchResultEl = document.getElementById("searchResultDiv");
+
+console.log(searchResultEl);
+
 function formSubmitHandler (keyWord) {
     console.log(keyWord);
 }
 
+function combineSearch (Title, Year, Type, Poster) {
+    if (Poster == "N/A"){
+        Poster = "./assets/no-poster.jpeg";
+    }
+    divHTML = ('<div class="searchResult"> <ul><li><img src="' + Poster + '" alt="Movie Poster" width="200"></li><li>Title: ' + Title + '</li> <li>Year: ' + Year + '</li> <li> Type: ' + Type + '</li></ul></div>');
+    return divHTML;
+};
+
 function getSearchResult (keyWord){
     var searchUrl = "https://www.omdbapi.com/?apikey=5972139b&r=json&s=" + keyWord;
     console.log(searchUrl);
+    var divHTML = "";
     fetch (searchUrl)
         .then(function (response){
             if (response.ok){
                 console.log(response);
                 response.json().then(function (data){
                     for (let i = 0; i < data.Search.length; i++) {
-                        var dataItem = data.Search[i];
-                        var title = dataItem.Title;
-                        var year = dataItem.Year;
-                        var imdbID = dataItem.imdbID;
-                        var type = dataItem.Type;
-                        var poster = dataItem.Poster;
-                                                
+                        var {Title, Year, Type, imdbID, Poster} = data.Search[i];
+                        divHTML = divHTML.concat(combineSearch(Title, Year, Type, Poster));
+                        console.log(Title);
+                        console.log(Year);
+                        console.log(Type);
+                        console.log(imdbID);
+                        console.log(Poster);
+                        console.log(divHTML);
                     };
-
+                    searchResultEl.innerHTML = divHTML;
+                    console.log(divHTML);
                 });
             };
         });
+
 };
 
 function openSearchResult (imdbID){
@@ -33,33 +49,22 @@ function openSearchResult (imdbID){
             if (response.ok){
                 console.log(response);
                 response.json().then(function (data){
-                        var title = data.Title;
-                        var releasedate = data.Released;
-                        var type = data.Type;
-                        var language = data.Language;
-                        var runtime = data.Runtime;
-                        var director = data.Director;
-                        var actors = data.Actors;
-                        var poster = data.Poster;
-                        var votes = data.imdbVotes;
-                        var boxoffice = data.BoxOffice;   
-                        var plot = data.Plot;
-                        var ratings = data.Ratings;
-                        for (let i = 0; i < ratings.length; i++) {
-                            var movieScore = ratings[i].Source + ": " + ratings[i].Value;
+                        var {Title, Released, Type, Language, Runtime, Director, Actors, Poster, imdbVotes, BoxOffice, Plot, Ratings} = data;
+                        for (let i = 0; i < Ratings.length; i++) {
+                            var movieScore = Ratings[i].Source + ": " + Ratings[i].Value;
                             console.log(movieScore);
                         };
-                        console.log(title);
-                        console.log(releasedate);
-                        console.log(type);
-                        console.log(language);
-                        console.log(runtime);
-                        console.log(director);
-                        console.log(actors);
-                        console.log(poster);
-                        console.log(votes);
-                        console.log(boxoffice);
-                        console.log(plot);
+                        console.log(Title);
+                        console.log(Released);
+                        console.log(Type);
+                        console.log(Language);
+                        console.log(Runtime);
+                        console.log(Director);
+                        console.log(Actors);
+                        console.log(Poster);
+                        console.log(imdbVotes);
+                        console.log(BoxOffice);
+                        console.log(Plot);
                     ;
 
                 });
